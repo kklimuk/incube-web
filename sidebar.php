bc6a9ad86bab1c0aef33451408a26f0babb325fe
stephenalexbrowne <stephenalexbrowne@gmail.com>
2012-08-21 19:20:00 -0400

    Add logo to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,10 @@
 <aside>
-	<div class="logo">
-	</div>
+	<div class="logo-container">
+		<a href="#" class="logo">
+			<div class="logo-center"> </div>
+			<div class="logo-top-right-corner"> </div>
+			<div class="logo-bottom-left-corner"> </div>
+		</a>
+		<div class="clear" style="clear:both"></div>
+	<div class="logo-container">
 </aside>
