b2a3ed40bb17ef77434a493eb410e09ca1ae56c9
Kirill Klimuk <kklimuk@gmail.com>
2012-09-06 14:10:10 -0400

    google analytics

--- header.php
+++ header.php
@@ -26,3 +26,16 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <script src="<?php bloginfo('template_directory'); ?>/js/jquery.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/totop.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34599790-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
