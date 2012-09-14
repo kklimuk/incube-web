2401fc68db85124e4214b993222b1759fda85965
Kirill Klimuk <kklimuk@gmail.com>
2012-09-14 11:56:13 -0400

    a really ghetto solution to the members

--- pages/members.php
+++ pages/members.php
@@ -1,9 +1,16 @@
 <?php
 global $wpdb;
-$query = "SELECT DISTINCT ID AS ID, (SELECT post_title FROM wp_posts WHERE ID=members.ID AND post_date_gmt=members.post_date_gmt) AS post_title FROM ((SELECT wp_users.ID AS ID, post_date_gmt FROM wp_users, wp_posts WHERE post_status = 'publish' AND post_author=wp_users.id  ORDER BY post_date_gmt DESC) UNION (SELECT ID, '1800-01-01' AS post_date_gmt FROM wp_users)) AS members GROUP BY ID ORDER BY post_date_gmt DESC;";
+$query = "SELECT ID, post_title, post_date_gmt FROM ((SELECT wp_users.ID, post_title, post_date_gmt FROM wp_users, wp_posts WHERE post_status = 'publish' AND post_author=wp_users.id  ORDER BY post_date_gmt DESC) UNION (SELECT ID, '' AS post_title, '1800-01-01' AS post_date FROM wp_users)) AS members ORDER BY post_date_gmt DESC;";
 $authors = $wpdb->get_results($query);
 
+$used = array();
 foreach($authors as $author) :
+	if (in_array($author->ID, $used)):
+		continue;
+	else:
+		$used[] = $author->ID;
+	endif;
+
 	$curauth = get_userdata($author->ID);
 	$user_link = get_author_posts_url($curauth->ID);
 	$description = get_the_author_meta('description', $author->ID);
